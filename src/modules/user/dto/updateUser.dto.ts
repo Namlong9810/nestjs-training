8ab1417e96b3a83ed74294d049edb241f9c4e401
Nam Ho Manh <namhm@its.local>
2025-02-26 17:57:59 +0700

    remove unuse code 2

--- src/modules/user/dto/updateUser.dto.ts
+++ src/modules/user/dto/updateUser.dto.ts
@@ -23,19 +23,7 @@ export class UpdateUserDTO {
   @IsEmail()
   email: string;
 
-  @ApiPropertyOptional({
-    example: 'Honhulong@9810',
-    description: 'Change user password(Optional) ',
-  })
   @IsOptional()
-  @MinLength(8, { message: 'Password required at least 8 chars' })
-  @Matches(/\d/, { message: 'password must include at least 1 number' })
-  @Matches(/[A-Z]/, { message: 'Password must include at least 1 Upcase char' })
-  @Matches(/[@!@#$%^&*?_]/, {
-    message: 'Password must Include at least 1 special char',
-  })
-  password: string;
-
   @IsEnum(Role)
   roles: Role;
 }
